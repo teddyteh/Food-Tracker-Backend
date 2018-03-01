48480d76482ff2904639a94d41ec5b1293ec6d6b
Teddy <teddy.teh@tecsico.com.au>
2018-03-01 22:04:24 +1100

    - Added production configuration

--- api/config/index.js
+++ api/config/index.js
@@ -1,9 +1,51 @@
 var path = require('path'),
     rootPath = path.normalize(__dirname + '/..'),
-    env = process.env.NODE_ENV || 'development';
+    env = process.env.NODE_ENV || 'development'; // default to development if NODE_ENV environment variable is not set
 
 var config = {
     development: {
+        root: rootPath,
+        app: {
+            name: 'food-tracker'
+        },
+        port: 3000,
+        db: {
+            base: "food_tracker",
+            user: "root",
+            pass: "Testest!2",
+            options: {
+                host: 'localhost',
+                port: 8887,
+                dialect: 'mysql',
+
+                pool: {
+                    max: 5,
+                    min: 0,
+                    acquire: 30000,
+                    idle: 10000
+                },
+
+                define: {
+                    underscored: true
+                }
+            }
+        },
+        force: true,
+        secret: 'ilovefoodtracking',
+        facebook: {
+            clientID: '1551657021555940',
+            clientSecret: '41c937034b0407d8e6fcd335b5c9e322',
+            callbackURL: 'http://localhost:3000/api/auth/facebook/callback',
+            profileFields: ['id', 'name', 'displayName', 'picture', 'email'],
+        },
+        google: {
+            clientID: '1041270276579-667cgpo6qhp5pbjjhtrunkh6cm61ef5g.apps.googleusercontent.com',
+            clientSecret: 'yJUnqyOA8pUF9TCB4acqytuw',
+            callbackURL: 'http://localhost:3000/api/auth/google/callback',
+        }
+    },
+
+    production: {
         root: rootPath,
         app: {
             name: 'food-tracker'
